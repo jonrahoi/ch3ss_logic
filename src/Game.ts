1ffe0d647c7cf32fdb36c2ebdf9518ca22b6f0a8
frederick08 <#R@sv8ui!G>
2019-07-02 15:59:35 -0700

    added moslty function shells

--- src/Game.ts
+++ src/Game.ts
@@ -1,12 +1,37 @@
 // game contains a board (with pieces)
-// game contains history of moves
+// game writes to JSON file for history of moves
 
 class Game {
 
+    moves = 0;
     board = new Board();
 
     //TODO play, etc
 
     // TODO check whose turn it is
+
+    // Create a new game
+    newGame() {
+        this.board = new Board();
+    }
+
+    // TODO
+    move(spaceA: string, spaceB: string): boolean {
+        return true;
+    }
+
+    // TODO
+    loadGame(arrayFromMoves: [], arrayToMoves: []) {
+        // go through array and move the board each space
+    }
+
+    saveGame() {
+    }
+
+    goBackOneMove() {
+    }
+
+    goForwardOneMove() {
+    }
 }
 
