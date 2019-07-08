c096913c252e25ad9ed509535967b4e610abb84b
frederick08 <#R@sv8ui!G>
2019-07-08 14:33:55 -0700

    piece starting positions

--- src/Board.ts
+++ src/Board.ts
@@ -12,22 +12,57 @@ export default class Board {
         return [
             // Level E Black
             // King Ec5; Rook Ea5, Ee5; Knight Eb5, Ed5; Pawn Ea4, Eb4, Ec4; Ed4; Ee4
-            new King("Black", 5, 3, 5),
+            new King("Black", 3, 5, 5),
+            new Rook("Black", 1, 5, 5),
+            new Rook("Black", 5, 5, 5),
+            new Knight("Black", 2, 5, 5),
+            new Knight("Black", 4, 5, 5),
+            new Pawn("Black", 1, 4, 5),
+            new Pawn("Black", 2, 4, 5),
+            new Pawn("Black", 3, 4, 5),
+            new Pawn("Black", 4, 4, 5),
+            new Pawn("Black", 5, 4, 5),
 
-            // Queens
-            //
+            // Level D Black
+            // Queen Dc5; Bishop Da5, Dd5; Unicorn Db5, De5; Pawn Da4, Db4, Dc4; Dd4; De4
+            new Queen("Black", 3, 5, 4),
+            new Bishop("Black", 1, 5, 4),
+            new Bishop("Black", 4, 5, 4),
+            new Unicorn("Black", 2, 5, 4),
+            new Unicorn("Black", 5, 5, 4),
+            new Pawn("Black", 1, 4, 4),
+            new Pawn("Black", 2, 4, 4),
+            new Pawn("Black", 3, 4, 4),
+            new Pawn("Black", 4, 4, 4),
+            new Pawn("Black", 5, 4, 4),
 
-            // Bishops
+            // Level C Empty at beginning
 
-            // Unicorns
+            // Level B White
+            // Queen Bc1; Bishop Ba1, Bd1; Unicorn Bb1, Be1; Pawn Ba2, Bb2, Bc2; Bd2; Be2
+            new Queen("Black", 3, 1, 2),
+            new Bishop("Black", 1, 1, 2),
+            new Bishop("Black", 4, 1, 2),
+            new Unicorn("Black", 2, 1, 2),
+            new Unicorn("Black", 5, 1, 2),
+            new Pawn("Black", 1, 2, 2),
+            new Pawn("Black", 2, 2, 2),
+            new Pawn("Black", 3, 2, 2),
+            new Pawn("Black", 4, 2, 2),
+            new Pawn("Black", 5, 2, 2),
 
-            // Knights
-
-
-            // Rooks
-
-            // Pawns
-
+            // Level A White
+            // King Ac1; Rook Aa1, Ae1; Knight Ab1, Ad1; Pawn Aa2, Ab2, Ac2; Ad2; Ae2
+            new King("Black", 3, 1, 1),
+            new Rook("Black", 1, 5, 1),
+            new Rook("Black", 5, 5, 1),
+            new Knight("Black", 2, 5, 1),
+            new Knight("Black", 4, 5, 1),
+            new Pawn("Black", 1, 4, 1),
+            new Pawn("Black", 2, 4, 1),
+            new Pawn("Black", 3, 4, 1),
+            new Pawn("Black", 4, 4, 1),
+            new Pawn("Black", 5, 4, 1),
         ]
     }
 
