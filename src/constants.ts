5016543132ab4fc8dafe01e8d7423d5e52cf419a
frederick08 <#R@sv8ui!G>
2019-08-02 17:18:06 -0700

    added Black White string constants

--- src/constants.ts
+++ src/constants.ts
@@ -2,4 +2,6 @@ export const RAUMSCHACH = "Raumschach";
 export const RA_SIZE_BOARD_X = 5;
 export const RA_SIZE_BOARD_Y = 5;
 export const RA_SIZE_BOARD_Z = 5;
+export const white = "White";
+export const black = "Black";
 
